55702d0e90805fb1ba94b8da8937fdb52f886072
Z User <z@container>
2026-07-05 13:44:05 +0000

    Add live cam banner to cams page

--- src/components/cantina/LiveCamsRoom.tsx
+++ src/components/cantina/LiveCamsRoom.tsx
@@ -76,6 +76,18 @@ export function LiveCamsRoom({ district }: LiveCamsRoomProps) {
         </div>
 
         <div className="livecams-encounters">
+          <a
+            href="https://t.camsk1.com/413627/9776/37445?aff_sub=CAMS&source=CANTINA&po=6533&aff_sub5=SF_006OG000004lmDN"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="livecams-encounter-card no-underline"
+          >
+            <img
+              src="https://www.imglnkx.com/9776/PCAM-244_DESIGN-24826_300250_2_EN.gif"
+              alt="LIVE CAMS"
+              style={{ width: '100%', height: 'auto', objectFit: 'contain', display: 'block' }}
+            />
+          </a>
           {encounters.length > 0 ? (
             encounters.map(({ resident, href }) => (
               <a
